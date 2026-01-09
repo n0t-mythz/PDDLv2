1844ae3a5b1055f1e7454863fe322ecfae6af1bf
Mythz <mythicgamer153@gmail.com>
2026-01-09 11:34:55 +0000

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -7,5 +7,5 @@ export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
-    { path: '/', component: Packs },
+    { path: '/packs', component: Packs },
 ];
